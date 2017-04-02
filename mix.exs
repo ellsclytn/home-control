b0fb427ebf038eda6d5786f5a44aeee51c806002
Ellis Clayton <ellis@ellisclayton.info>
2017-04-02 15:29:33 +1000

    Add live reload

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule Thermio.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:joken, "~> 1.1"}]
+     {:joken, "~> 1.1"},
+     {:phoenix_live_reload, "~> 1.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
