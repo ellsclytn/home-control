883f1f85b1fcfef9f31c890ba33211d983117b07
Ellis Clayton <ellis@ellisclayton.info>
2017-10-28 17:07:17 +1000

    Remove redundant applications list

--- mix.exs
+++ mix.exs
@@ -18,8 +18,7 @@ defmodule Thermio.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Thermio, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :timex, :httpoison]]
+     extra_applications: [:logger]]
   end
 
   # Specifies which paths to compile per environment.
